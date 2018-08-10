b7d515bbf43864747b3d56effbfda4fef320bddb
nbarnes <nbarnes@gmail.com>
2018-08-10 14:34:38 -0700

    prop pixel offsets were wrong, making them appear in the wrong place.  Fixed.

--- src/scene_definition.js
+++ src/scene_definition.js
@@ -39,37 +39,37 @@ export const sceneDef = {
       imagePaths: ['./src/img/props/statue1-01.png'],
       startCell: { x: 5, y: 5 },
       frameSize: { width: 128, height: 128 },
-      frameOffsets: { x: 0, y: 14 }
+      frameOffsets: { x: 64, y: 112 }
     },
     {
       imagePaths: ['./src/img/props/statue1-02.png'],
       startCell: { x: 0, y: 3 },
       frameSize: { width: 128, height: 128 },
-      frameOffsets: { x: 0, y: 14 }
+      frameOffsets: { x: 64, y: 112 }
     },
     {
       imagePaths: ['./src/img/props/statue1-03.png'],
       startCell: { x: 4, y: 1 },
       frameSize: { width: 128, height: 128 },
-      frameOffsets: { x: 0, y: 14 }
+      frameOffsets: { x: 64, y: 112 }
     },
     {
       imagePaths: ['./src/img/props/statue1-04.png'],
       startCell: { x: 5, y: 1 },
       frameSize: { width: 128, height: 128 },
-      frameOffsets: { x: 0, y: 14 }
+      frameOffsets: { x: 64, y: 112 }
     },
     {
       imagePaths: ['./src/img/props/statue1-02.png'],
       startCell: { x: 6, y: 0 },
       frameSize: { width: 128, height: 128 },
-      frameOffsets: { x: 0, y: 14 }
+      frameOffsets: { x: 64, y: 112 }
     },
     {
       imagePaths: ['./src/img/props/statue1-03.png'],
       startCell: { x: 7, y: 9 },
       frameSize: { width: 128, height: 128 },
-      frameOffsets: { x: 0, y: 14 }
+      frameOffsets: { x: 64, y: 112 }
     }
   ]
 };
